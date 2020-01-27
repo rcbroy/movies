3678f379fbb4360f6b84ff5eaeb8d91e78a5a76c
Roberto Broy <rcbroy@gmail.com>
2020-01-27 12:39:38 -0800

    add search and results page

--- moviesapp/urls.py
+++ moviesapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('movies', views.movies, name='movies'),
+    path('results', views.results, name='results'),
 ]
--- moviesapp/views.py
+++ moviesapp/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from .tmdb import TMDB
 
+tmdb = TMDB()
 def home(request):
     return render(request, 'moviesapp/home.html', {})
 
 def movies(request):
-    tmdb = TMDB()
     results = tmdb.movie_search("star wars")
+    print(type(results))
     # data = simplejson.dumps(results)
     return JsonResponse(results)
     # return HttpResponse(data, content_type='application/json')
+
+def results(request):
+    query = request.GET['query']
+    response = tmdb.movie_search(query)
+    return render(request, 'moviesapp/results.html', response)
